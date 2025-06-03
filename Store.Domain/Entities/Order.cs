14d87935d944f4599642533885ad237f0846738a
ronaldo <ronaldo.dsantos@gmail.com>
2025-06-02 23:54:53 -0300

    first commit

---
+++ Store.Domain/Entities/Order.cs
@@ -0,0 +1,56 @@
+﻿using Store.Domain.Enums;
+
+namespace Store.Domain.Entities
+{
+    public class Order : Entity
+    {
+        public Order(Customer customer, decimal deliveryFree, Discount discount)
+        {
+            Customer = customer;
+            Date = DateTime.Now;
+            Number = Guid.NewGuid().ToString().Substring(0, 8);
+            Status = EOrderStatus.WaitingPayment;
+            DeliveryFree = deliveryFree;
+            Discount = discount;
+            Items = new List<OrderItem>();
+        }
+
+        public Customer Customer { get; private set; }
+        public DateTime Date { get; private set; } 
+        public string Number { get; private set; }
+        public IList<OrderItem> Items { get; private set; } 
+        public decimal DeliveryFree { get; private set; }
+        public Discount Discount { get; private set; }
+        public EOrderStatus Status { get; private set; }
+
+        public void AddItem(Product product, int quantity)
+        {
+            var item = new OrderItem(product, quantity);
+        }
+
+        public decimal Total()
+        {
+            decimal total = 0;
+            foreach (var item in Items)
+            {
+                total += item.Total();
+            }
+
+            total += DeliveryFree;
+            total -= Discount != null ? Discount.Value() : 0;
+
+            return total;
+        }
+
+        public void Pay(decimal amount)
+        {
+            if (amount == Total())
+                this.Status = EOrderStatus.WaitingDelivery;
+        }
+
+        public void Cancel()
+        {
+            Status = EOrderStatus.Canceled;
+        }
+    }
+}
