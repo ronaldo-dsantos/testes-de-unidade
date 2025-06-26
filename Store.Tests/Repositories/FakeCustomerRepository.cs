80d1985d08653281fa1443d4ea40da20619c6d60
ronaldo <ronaldo.dsantos@gmail.com>
2025-06-25 23:05:57 -0300

    test: testandos queries

---
+++ Store.Tests/Repositories/FakeCustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Store.Domain.Entities;
+using Store.Domain.Repositories;
+
+namespace Store.Tests.Repositories
+{
+    public class FakeCustomerRepository : ICustomerRepository
+    {
+        public Customer Get(string document)
+        {
+            if (document == "12345678910")
+                return new Customer("Ronaldo Domingues", "ronaldo@email.com");
+
+            return null;
+        }
+    }
+}
---
+++ Store.Tests/Repositories/FakeDelivereFeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Store.Domain.Repositories;
+
+namespace Store.Tests.Repositories
+{
+    public class FakeDelivereFeeRepository : IDeliveryFeeRepository
+    {
+        public decimal Get(string zipCode)
+        {
+            return 10;
+        }
+    }
+}
